8d6adc56f84dc21eb607ab4fdc3af962d2cc4bac
jeongjoonsup <penny4860@gmail.com>
2017-08-21 21:54:53 +0900

    11: image data augmentation (flip)

--- model.py
+++ model.py
@@ -2,6 +2,17 @@ import cv2
 import numpy as np
 import csv
 
+def augment_data(images, measurements):
+    augment_images = []
+    augment_measurements = []
+    
+    for img, meas in zip(images, measurements):
+        augment_images.append(img)
+        augment_measurements.append(meas)
+        augment_images.append(cv2.flip(img, 1))
+        augment_measurements.append(-1.0*meas)
+    return augment_images, augment_measurements
+
 lines = []
 with open("dataset/1/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
@@ -18,6 +29,10 @@ for line in lines:
     images.append(image)
     
     measurements.append(float(line[3]))
+
+print(len(images))
+images, measurements = augment_data(images, measurements)
+print(len(images))
     
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -26,25 +41,25 @@ from keras.models import Sequential
 from keras.layers.convolutional import Convolution2D
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import MaxPooling2D, Activation
-
-
  
+ 
+  
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-
+ 
 model.add(Convolution2D(8, 5, 5,
                         border_mode='same'))
 # model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+ 
 model.add(Convolution2D(8, 5, 5,
                         border_mode='same'))
-
+ 
 # model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+ 
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation('relu'))
@@ -52,11 +67,11 @@ model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dense(1))
 model.summary()
-
-
+ 
+ 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-  
+   
 model.save('model.h5')
 
 
