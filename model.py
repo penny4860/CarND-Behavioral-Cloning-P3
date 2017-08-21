8764024fe39938336b742d84a6a35841d6a51644
jeongjoonsup <penny4860@gmail.com>
2017-08-22 00:44:42 +0900

    Revert "batchnorm"
    This reverts commit cb3bff280f8d44c3325ccbe0047bc6795968f878.

--- model.py
+++ model.py
@@ -65,7 +65,8 @@ from keras.models import Sequential
 from keras.layers.convolutional import Convolution2D
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import MaxPooling2D, Activation, Cropping2D
-from keras.layers.normalization import BatchNormalization
+ 
+ 
   
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -73,23 +74,22 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((50,20), (0,0))))
 model.add(Convolution2D(8, 5, 5,
                         border_mode='same'))
-model.add(BatchNormalization())
+# model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
  
 model.add(Convolution2D(8, 5, 5,
                         border_mode='same'))
-model.add(BatchNormalization())
+ 
+# model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
  
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation('relu'))
-model.add(BatchNormalization())
 model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(BatchNormalization())
 model.add(Dense(1))
 model.summary()
  
