bca01d74772bdc7e0cc19b2ac5f5d7ac07748b48
jeongjoonsup <penny4860@gmail.com>
2017-08-21 21:34:43 +0900

    10: more network (convnet layer added)

--- model.py
+++ model.py
@@ -23,16 +23,40 @@ X_train = np.array(images)
 y_train = np.array(measurements)
  
 from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import MaxPooling2D, Activation
+
+
  
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+model.add(Convolution2D(8, 5, 5,
+                        border_mode='same'))
+# model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(8, 5, 5,
+                        border_mode='same'))
+
+# model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+model.add(Dense(128))
+model.add(Activation('relu'))
 model.add(Dense(1))
- 
+model.summary()
+
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
- 
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+  
 model.save('model.h5')
 
 
