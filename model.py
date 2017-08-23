54cb18d02aa1794adbf4e4c9f2802c301e743165
jeongjoonsup <penny4860@gmail.com>
2017-08-23 21:36:02 +0900

    17 : Generator

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import csv
+from sklearn.model_selection import train_test_split
+import sklearn
+from random import shuffle
+
 
 def augment_data(images, measurements):
     augment_images = []
@@ -63,31 +67,45 @@ def plot_history(history_object):
     plt.show()
     
 
+def generator(samples, batch_size=32, do_augment=True, use_side_data=True):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                images, angles = get_samples_in_line(batch_sample, images, angles)  # (x3)
+                
+            images, angles = augment_data(images, angles)                           # (x2)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
 lines = []
 with open("dataset/1/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
-images = []
-measurements = []
-for line in lines:
-    images, measurements = get_samples_in_line(line, images, measurements)    
+train_lines, validation_lines = train_test_split(lines, test_size=0.2)
 
-print(len(images))
-images, measurements = augment_data(images, measurements)
-print(len(images))
-    
-X_train = np.array(images)
-y_train = np.array(measurements)
- 
+# compile and train the model using the generator function
+train_generator = generator(train_lines, batch_size=20)
+validation_generator = generator(validation_lines, batch_size=120, do_augment=False, use_side_data=False)
+
+
 from keras.models import Sequential
 from keras.layers.convolutional import Convolution2D
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import MaxPooling2D, Activation, Cropping2D
- 
- 
-  
+   
+   
+    
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 # (top_crop, bottom crop), (left, right)
@@ -97,14 +115,14 @@ model.add(Convolution2D(8, 5, 5,
 # model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
- 
+   
 model.add(Convolution2D(8, 5, 5,
                         border_mode='same'))
- 
+   
 # model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
- 
+   
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation('relu'))
@@ -112,15 +130,19 @@ model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dense(1))
 model.summary()
- 
- 
-model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2, verbose=2)
-plot_history(history_object)
    
+   
+model.compile(loss='mse', optimizer='adam')
+# history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2, verbose=2)
+ 
+history_object = model.fit_generator(train_generator,
+                                     samples_per_epoch=len(train_lines)*6,
+                                     validation_data=validation_generator,
+                                     nb_val_samples=len(validation_lines),
+                                     nb_epoch=2,
+                                     verbose=2)
+ 
+plot_history(history_object)
 model.save('model.h5')
-
-
-
 
 
