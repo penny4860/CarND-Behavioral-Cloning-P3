af67ad025647dca0507ce7616b2d9aad54dd378e
jeongjoonsup <penny4860@gmail.com>
2017-08-27 21:24:22 +0900

    Revert "nit"
    This reverts commit 00eaac3b88ea7c8d8ebbbc306b0a5ba2050c814c.

--- creaate_ann_script.py
+++ creaate_ann_script.py
@@ -14,35 +14,30 @@ LOG_FILES = ['C://Users//joonsup//git//dataset//logs//1.csv',
 ANNOTATION_FILE = 'annotation.json'
 STEERING_COEFFICIENT = 0.229
 
-def get_files(log_files):
+def get_files(log_file):
+    data = pd.read_csv(log_file)
     
-    image_files = []
-    targets = []
-    for log_file in log_files:
-        data = pd.read_csv(log_file)
-        
-        image_files += (data['center'].tolist() + data['left'].tolist() + data['right'].tolist())
-
-        targets.append(np.array(data['steering']))
-        targets.append(np.array(data['steering']) + STEERING_COEFFICIENT)
-        targets.append(np.array(data['steering']) - STEERING_COEFFICIENT)
-        
+    image_files = data['center'].tolist() + data['left'].tolist() + data['right'].tolist()
+    
     for i, filename in enumerate(image_files):
         image_files[i] = os.path.basename(filename)
+    
+    targets = [np.array(data['steering']), 
+               np.array(data['steering']) + STEERING_COEFFICIENT,
+               np.array(data['steering']) - STEERING_COEFFICIENT]
     targets = np.concatenate(targets, axis=0).tolist()
-
     return image_files, targets
 
 
 if __name__ == "__main__":
     # 1. Get image files & target labels    
-    img_files, targets = get_files(LOG_FILES)
+    img_files, targets = get_files(LOG_FILE)
     
     # 2. Collect annotations
     annotations = []
     for filename, target in zip(img_files, targets):
         annotations.append({"filename": filename, "target": target})
-     
+    
     # 3. Write to annotation files
     with open('annotation.json', 'w') as fp:
         json.dump(annotations, fp, indent=4)
