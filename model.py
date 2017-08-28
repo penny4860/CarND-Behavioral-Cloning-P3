6946aa8f822c61158e88383883caea65e9f714a6
jeongjoonsup <penny4860@gmail.com>
2017-08-29 01:18:44 +0900

    tf.app.flags로 argument parser 기능 추가

--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 import pickle
 import cv2
 import json
+import tensorflow as tf
 
 from random import shuffle
 from keras.models import Sequential
@@ -13,6 +14,14 @@ from keras.layers import MaxPooling2D, Activation
 from generator.image_augment import CarAugmentor, NothingAugmentor
 from generator.image_preprocess import Preprocessor
 from generator.generator import DataGenerator
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('image_path', "C://Users//joonsup//git//dataset//images", 'Directory containing images')
+# flags.DEFINE_string('image_path', 'dataset//images', 'Directory containing images')
+flags.DEFINE_integer('n_epochs', 8, 'number of epochs')
+flags.DEFINE_float('training_ratio', 0.8, 'ratio of training samples')
 
 
 def build_model():
@@ -54,22 +63,19 @@ def build_model():
     model.compile(loss='mse', optimizer='adam')
     return model
 
-# Todo: args
-image_path = "C://Users//joonsup//git//dataset//images"
-number_of_epochs = 8
 if __name__ == "__main__":
     with open('annotation.json', 'r') as fp:
         anns = json.load(fp)
     shuffle(anns)
     
-    n_train_samples = int(len(anns)*0.8)
+    n_train_samples = int(len(anns)*FLAGS.training_ratio)
     train_annotations = anns[:n_train_samples]
     valid_annotations = anns[n_train_samples:]
 
     # validation generator : augment (x)
     # https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
-    test_data_generator = DataGenerator(image_path, valid_annotations, NothingAugmentor(), Preprocessor())
-    train_data_generator = DataGenerator(image_path, train_annotations, CarAugmentor(), Preprocessor())
+    test_data_generator = DataGenerator(FLAGS.image_path, valid_annotations, NothingAugmentor(), Preprocessor())
+    train_data_generator = DataGenerator(FLAGS.image_path, train_annotations, CarAugmentor(), Preprocessor())
 
     train_gen = train_data_generator.next_batch()
     validation_gen = test_data_generator.next_batch()
@@ -78,11 +84,11 @@ if __name__ == "__main__":
      
     history_object = model.fit_generator(train_gen,
                                          samples_per_epoch=len(train_annotations),
-                                         nb_epoch=number_of_epochs,
+                                         nb_epoch=FLAGS.n_epochs,
                                          validation_data=validation_gen,
                                          nb_val_samples=len(valid_annotations),
                                          verbose=1)
- 
+
     pickle.dump(history_object.history, open('training_history.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
     model.save('model.h5')
 
